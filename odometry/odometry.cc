60f636ac0db2dfc290ebebbe050c7fa66ee30be4
Benjamin <benjamin.kisliuk@dfki.de>
2018-07-09 17:27:21 +0000

    initial commit

---
+++ odometry/odometry.cc
@@ -0,0 +1,49 @@
+/* User code: This file will not be overwritten by TASTE. */
+
+#include "odometry.h"
+#include "base_support/Base-samples-BodyStateConvert.hpp"
+#include <base_support/Base-commands-Motion2DConvert.hpp>
+#include "base_support/OpaqueConversion.hpp"
+#include <cmath>
+
+asn1SccBase_samples_BodyState bs;
+
+void odometry_startup()
+{
+   base::Vector3d translation(1.0, 0.0, 0.0);
+   asn1Scc_Vector3d_toAsn1(bs.pose.translation, translation);
+}
+
+void odometry_PI_mc_in(const asn1SccBase_commands_Motion2D *IN_in_mc)
+{
+  base::commands::Motion2D base_mc;
+  base::Vector3d translation(0.0,0.0,0.0);
+  base::Quaterniond orientation;
+
+  asn1SccBase_commands_Motion2D_fromAsn1(base_mc, *IN_in_mc);
+ 
+  // get current values
+  asn1Scc_Vector3d_fromAsn1(translation, bs.pose.translation);
+  asn1Scc_Quaterniond_fromAsn1(orientation, bs.pose.orientation);
+  
+  float f_orientation = base::getYaw(orientation); 
+  float x = translation[0]; 
+  float y = translation[1];
+
+  // calculate new values
+
+  float x_ = base_mc.translation * cos(f_orientation);
+  float y_ = base_mc.translation * sin(f_orientation); 
+
+  base::Vector3d translation_(x+x_,y+y_,0.0);
+
+  float f_orientation_ = fmod((f_orientation + base_mc.rotation*M_PI/4.0),2*M_PI);
+
+  base::Quaterniond orientation_(base::AngleAxisd(f_orientation_, base::Vector3d::UnitZ()));
+
+  // new Body state
+  asn1Scc_Vector3d_toAsn1(bs.pose.translation, translation_);
+  asn1Scc_Quaterniond_toAsn1(bs.pose.orientation, orientation_);
+  odometry_RI_updateBodyState(&bs);
+}
+
---
+++ odometry/odometry.h
@@ -0,0 +1,25 @@
+/* This file was generated automatically: DO NOT MODIFY IT ! */
+
+/* Declaration of the functions that have to be provided by the user */
+
+#ifndef __USER_CODE_H_odometry__
+#define __USER_CODE_H_odometry__
+
+#include "C_ASN1_Types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void odometry_startup();
+
+void odometry_PI_mc_in(const asn1SccBase_commands_Motion2D *);
+
+extern void odometry_RI_updateBodyState(const asn1SccBase_samples_BodyState *);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
